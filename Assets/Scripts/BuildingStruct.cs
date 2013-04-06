5f35cd385e3e9d7a15dc7299277606ad61ad0a72
Jesmond Chia <chia.jesmond@gmail.com>
2013-04-06 03:44:49 -0400

    Build mechanism roughly in place

--- Assets/Scripts/BuildingStruct.cs
+++ Assets/Scripts/BuildingStruct.cs
@@ -4,7 +4,28 @@ using System.Collections;
 public class BuildingStruct : MonoBehaviour {
 	// All buildings should inherit this class
 	
-	public int moneyGenerated;
+	public int cost;
 	public int populationSupport;
 	public int pollutionIndex;
+	public bool isConnected;
+	public bool flag;
+	public Vector2 coordinates;
+	
+	public void Build(Vector2 coord) {
+		GameState.Instance.PlaceBuilding(this, coord);
+		coordinates = coord;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if(isConnected && !flag){
+			GameState.Instance.population += populationSupport;
+			flag = true;
+		}
+		
+		if(!isConnected && flag){
+			GameState.Instance.population -=populationSupport;
+			flag = false;
+		}
+	}
 }
--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -6,6 +6,11 @@ public class GameState : MonoBehaviour {
 	public int cash;
 	public int pollution;
 	public int population;
+	public BuildingStruct[,] grid;
+	public int gridSize;
+	public int tick;
+	
+	private bool cashTick;
 	
 	public static GameState MyInstance;
 	public static GameState Instance {
@@ -22,11 +27,43 @@ public class GameState : MonoBehaviour {
 		MyInstance = this;
 		cash = 20;
 		pollution = 0;
-		population = 0;
+		population = 1;
+		tick = 5;
+		
+		cashTick = true;
+		
+		grid = new BuildingStruct[gridSize,gridSize];
+	}
+	
+	public bool PlaceBuilding(BuildingStruct x, Vector2 coord) {
+		if(cash >= x.cost){
+			cash -= x.cost;
+			pollution += x.pollutionIndex;
+			grid[(int)coord.x,(int)coord.y] = x;
+			
+			print("Build successful");
+			
+			return true;
+		} else{
+			print("Insufficient Cash");
+			
+			return false;
+		}
+	}
+	
+	public int cashFlow () { //contains formula for how much money is generated
+		return population*20;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Time.time%tick < 0.1) { // condition for time interval
+			if(cashTick){
+				cash += cashFlow();
+				cashTick = false;
+			}
+		} else{
+			cashTick = true;
+		}
 	}
 }
